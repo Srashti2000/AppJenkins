5db5f98d68fc00201a0fabf17f06110a436f36cc
Srashti Jain <jainsrashti041@gmail.com>
2022-08-12 14:32:59 +0530

    Add project files.

---
+++ HelloApp/Calc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloApp
+{
+    internal class Calc
+    {
+        internal object Add(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Multiply(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
